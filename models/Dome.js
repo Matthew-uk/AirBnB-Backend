224c23aacc25822e4c8612aadb044bdf1742b031
The Primotion Studio <oyedelenewton@gmail.com>
2024-02-20 10:52:18 +0100

    Added Dome and Reviews Database Models

---
+++ models/Dome.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const DomeSchema = mongoose.Schema({
+	name: {
+		type: String,
+		required: true,
+	},
+	amenities: {
+		type: [{
+			electrical_utility: [{
+				type: String,
+				enum: ['TV', 'Refrigerator', 'Iron', 'Heater']
+			}],
+			furniture: Number,
+			bedroom: Number,
+			bathroom: Number,
+			kitchen: Number,
+			number: Number
+		}],
+		required: true,
+	},
+	state: {
+		type: String,
+		required: true,
+	},
+	city: {
+		type:
+	},
+	base_price_by_night: {
+		type: Number,
+		required: true,
+	},
+	description: {
+		type: String,
+		required: true,
+	},
+	// Short tags used to describe a particular dome
+	tags: {
+		type: [String],
+		required: true,
+	},
+});
+module.exports = mongoose.Model("Dome", DomeSchema);
